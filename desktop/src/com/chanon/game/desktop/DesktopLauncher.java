5ef64877c210524ff532ecbb90d1debeafa24928
chano <chano@10.2.59.40>
2016-09-23 15:45:25 +0700

    emthy screen

---
+++ desktop/src/com/chanon/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.chanon.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chanon.game.NimGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 800;
+		new LwjglApplication(new NimGame(), config);
+	}
+}
